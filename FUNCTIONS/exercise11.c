2e5659d25e2603a644af68a526827e86ef147586
Jennie547 <jessica4edigin@gmail.com>
2024-02-10 11:23:12 +0100

    fikes

---
+++ FUNCTIONS/exercise11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "exercise11.h"
+
+int sum(int a, int b){
+    return (a+b);
+}
+
+int main(void){
+
+    int num1, num2;
+
+    printf("Enter 2 whole numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int result = sum(num1, num2);
+
+    printf("%d + %d = %d\n", num1, num2, result);
+
+    return(0);
+}

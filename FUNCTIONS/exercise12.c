e23c703c60d93cba36328dc09f3e112459063996
Jennie547 <jessica4edigin@gmail.com>
2024-02-10 11:49:04 +0100

    files

---
+++ FUNCTIONS/exercise12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include "exercise12.h"
+
+#define PI 3.14
+
+double circleArea(double radius){
+
+    return(pow(radius, 2) * PI);
+}
+
+int main(void){
+
+    double radius;
+
+    printf("Enter any number: ");
+    scanf("%lf", &radius);
+
+    double area = circleArea(radius);
+
+    printf("%.2lf\n", area);
+
+    return(0);
+}
---
+++ FUNCTIONS/exercise12.h
@@ -0,0 +1,6 @@
+#ifndef EXERCISE12_H
+#define EXERCISE12_H
+
+double circleArea(double radius);
+
+#endif
